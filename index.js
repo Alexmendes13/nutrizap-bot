57a0618d48623c943db7d7c6a78f3807bc342279
Alexmendes13 <cybermendes@gmail.com>
2025-03-20 15:53:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-// Troca pelo teu token e instance id da Z-API
 const ZAPI_TOKEN = 'AFB512DDD2891F0B378EC8D4';
 const ZAPI_INSTANCE_ID = '3DE7C43498DEB07EA28332C54B267657';
 
-// Rota que recebe o webhook da Z-API
 app.post('/', async (req, res) => {
     const body = req.body;
     console.log('Recebido:', body);
@@ -25,15 +23,13 @@ app.post('/', async (req, res) => {
         });
         console.log('Mensagem enviada');
     } catch (err) {
-        console.error('Erro ao enviar:', err.response?.data);
+        console.error('Erro ao enviar:', err.response ? err.response.data : err.message);
     }
 
     res.sendStatus(200);
 });
 
-// Teste pra saber se o bot tá online
 app.get('/', (req, res) => res.send('NutriZap rodando 🔥'));
 
-// Subir o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
